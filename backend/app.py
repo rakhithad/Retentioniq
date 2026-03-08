cbe68ab5e07e63c6f5863c55c33c86532675c072
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2026-03-09 04:13:44 +0530

    cors for React

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # ---------- App setup ----------
 app = FastAPI(title="AttritionIQ Dashboard")
@@ -11,3 +12,11 @@ def root():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
