566d2e859f8badff65800fdb3714a03cd0f5e800
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2026-03-09 05:20:13 +0530

    added app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/Login";
+import EmployeeDataForm from "./components/EmployeeDataForm";
+import HRDashboard from "./components/HRDashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/form" element={<EmployeeDataForm />} />
+        <Route path="/hr-dashboard" element={<HRDashboard />} />
+      </Routes>
     </div>
   );
 }
